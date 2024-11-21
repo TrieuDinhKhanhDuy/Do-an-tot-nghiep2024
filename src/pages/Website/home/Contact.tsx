1d276c0c075b70979a80c07b0d14bf5a2e8dd2b9
TrieuDinhKhanhDuy <godofcaptain@gmail.com>
2024-11-21 16:06:30 +0700

    fix validation contact

--- src/pages/Website/home/Contact.tsx
+++ src/pages/Website/home/Contact.tsx
@@ -1,3 +1,6 @@
+
+
+
 import Breadcrumb from "@/components/Breadcrumb";
 import "../../../styles/Website/Contact.css";
 import { useForm } from "react-hook-form";
@@ -7,6 +10,7 @@ import ContactsType from "@/types/IContacts";
 import { useContact } from "@/hooks/useContact";
 
 const Contact = () => {
+
     const duongDan = [
         { nhan: 'Trang Chủ', duongDan: '/' },
         { nhan: 'Liên Hệ', duongDan: 'contact' },
@@ -22,7 +26,7 @@ const Contact = () => {
         message: z.string().nonempty("Nội dung liên hệ không được để trống"),
     });
 
-    const { register, handleSubmit, formState: { errors }, reset } = useForm<ContactsType>({
+    const { register, handleSubmit, formState: { errors, touchedFields }, reset } = useForm<ContactsType>({
         resolver: zodResolver(contactSchema),
     });
     const { handleAddContact } = useContact();
@@ -31,6 +35,7 @@ const Contact = () => {
         handleAddContact(data);
         reset();
     };
+
 
     return (
         <>
@@ -44,17 +49,32 @@ const Contact = () => {
                         <h3>CÔNG TY CPTM HONG NHUNG</h3>
                         <p>
                             Công ty CP Thương mại và du lịch Hồng Nhung được
-                            thành lập ngày 21/02/2004...
+                            thành lập ngày 21/02/2004. Với 7 chi nhánh, 24 văn
+                            phòng tại 4 tỉnh thành: Hà Nội, Thái Nguyên, Tuyên
+                            Quang, Bắc Kạn, gần 700 lao động và gần 400 phương
+                            tiện, đồng hành với hơn 10 triệu hành trình mỗi năm,
+                            Hà Lan đã và đang đổi mới mạnh mẽ hơn nữa để nâng
+                            cao hiệu quả, sức cạnh tranh của doanh nghiệp thị
+                            trường Tỉnh Thái Nguyên và trên cả nước. Cùng với
+                            việc đầu tư phát triển, mở rộng mạng lưới, tuyến
+                            mới, lộ trình mới và đầu tư cơ sở vật chất, những
+                            dòng xe chất lượng cao. Hà Lan còn tập trung và đẩy
+                            mạnh áp dụng công nghệ và chuyển đổi số vào hoạt
+                            động sản xuất kinh doanh. Địa chỉ: 271 – 273 Dương
+                            Tự Minh, P.Tân Long, Tp.Thái Nguyên. Xe hợp đồng:
+                            0989 759 759. Chuyển phát nhanh: 1900 0092. Tuyển
+                            dụng: 0977 306 567. Email:
+                            phungthihongnhung21@gmail.com.
                         </p>
 
                         <form className="contactForm-form needs-validation" onSubmit={handleSubmit(onSubmit)} noValidate>
-                            <div className="contactForm-row">
+                            <div className="contactForm-row">   
                                 <div className="contactForm-group mgleft-5px">
                                     <label>Họ và tên:</label>
                                     <input
                                         type="text"
                                         placeholder="Nhập họ và tên"
-                                        className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+                                        className={`form-control ${errors.name ? 'is-invalid' : ''} ${touchedFields.name && !errors.name && 'is-valid'}`}
                                         {...register("name")}
                                     />
                                     {errors.name && <div className="invalid-feedback">{errors.name.message}</div>}
@@ -65,7 +85,7 @@ const Contact = () => {
                                     <input
                                         type="text"
                                         placeholder="Nhập số điện thoại"
-                                        className={`form-control ${errors.phone ? 'is-invalid' : ''}`}
+                                        className={`form-control ${errors.phone ? 'is-invalid' : ''} ${touchedFields.phone && !errors.phone && 'is-valid'}`}
                                         {...register("phone")}
                                     />
                                     {errors.phone && <div className="invalid-feedback">{errors.phone.message}</div>}
@@ -77,7 +97,7 @@ const Contact = () => {
                                 <input
                                     type="email"
                                     placeholder="Nhập email"
-                                    className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                                    className={`form-control ${errors.email ? 'is-invalid' : ''} ${touchedFields.email && !errors.email && 'is-valid'}`}
                                     {...register("email")}
                                 />
                                 {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
@@ -88,7 +108,7 @@ const Contact = () => {
                                 <input
                                     type="text"
                                     placeholder="Nhập tiêu đề"
-                                    className={`form-control ${errors.title ? 'is-invalid' : ''}`}
+                                    className={`form-control ${errors.title ? 'is-invalid' : ''} ${touchedFields.title && !errors.title && 'is-valid'}`}
                                     {...register("title")}
                                 />
                                 {errors.title && <div className="invalid-feedback">{errors.title.message}</div>}
@@ -98,20 +118,26 @@ const Contact = () => {
                                 <label>Nội dung liên hệ:</label>
                                 <textarea
                                     placeholder="Nhập nội dung của bạn"
-                                    className={`form-control ${errors.message ? 'is-invalid' : ''}`}
+                                    className={`form-control ${errors.message ? 'is-invalid' : ''} ${touchedFields.message && !errors.message && 'is-valid'}`}
                                     {...register("message")}
                                 ></textarea>
                                 {errors.message && <div className="invalid-feedback">{errors.message.message}</div>}
                             </div>
 
-                            <button type="submit" className="contactForm-button btn btn-primary">
+                            <button type="submit" className="contactForm-button">
                                 GỬI THƯ
                             </button>
                         </form>
                     </div>
 
                     <div className="contactForm-map">
-                        <iframe src="https://www.google.com/maps/embed?pb=..." width="100%" height="100%" style={{ border: '0' }} allowFullScreen={true} loading="lazy"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.1063645750455!2d105.77569377572404!3d21.02842968062079!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b2f431c099%3A0xe44043bacd461128!2zQuG6v24gWGUgTeG7uSDEkMOsbmg!5e0!3m2!1svi!2s!4v1728287236739!5m2!1svi!2s"
+                            width="100%"
+                            height="100%"
+                            style={{ border: '0' }}
+                            allowFullScreen={true}
+                            loading="lazy"
+                        ></iframe>
                     </div>
                 </div>
             </div>
